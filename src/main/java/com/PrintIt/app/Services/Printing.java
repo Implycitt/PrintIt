0ff7141a4c877facfd41762c1677816af1c9907f
Quentin Bordelon <qgbordelon@gmail.com>
2025-10-30 11:00:16 -0500

    feat: printing basic functions

--- src/main/java/com/PrintIt/app/Services/Printing.java
+++ src/main/java/com/PrintIt/app/Services/Printing.java
@@ -1,8 +1,38 @@
 package com.PrintIt.app.Services;
 
+import java.util.*;
+import java.awt.print.PrinterJob;
+
 public class Printing
 {
-  public static void print()
+
+  static Queue<String> printerQueue = new ArrayDeque<String>();
+
+  public void addToQueue(String path)
+  {
+    printerQueue.add(path);
+  }
+
+  public void removeFromQueue()
+  {
+    printerQueue.remove();
+  }
+
+  public void startPrintQueue()
+  {
+    while (printerQueue.size() != 0)
+    {
+      printSingle();
+      removeFromQueue();
+    }
+  }
+
+  public static void printSingle()
+  {
+    //TODO
+  }
+
+  public static void setPrinterProperties()
   {
     // TODO
   }
