6ee16db090ef1e4e0cd3ee85740c059d2b4b5acf
Quentin Bordelon <qgbordelon@gmail.com>
2025-11-13 12:07:44 -0600

    feat: printing

--- src/main/java/com/PrintIt/app/Services/Printing.java
+++ src/main/java/com/PrintIt/app/Services/Printing.java
@@ -1,21 +1,31 @@
 package com.PrintIt.app.Services;
 
 import java.util.*;
+
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.printing.PDFPageable;
+
+import javafx.print.PaperSource;
+import javafx.print.PrinterAttributes;
+
 import java.awt.print.PrinterJob;
+import java.io.File;
 
 public class Printing
 {
 
-  static Queue<String> printerQueue = new ArrayDeque<String>();
+  public static Queue<String> printerQueue = new ArrayDeque<String>();
+  public static PrinterJob printJob = PrinterJob.getPrinterJob();
+  public static PrintService defaultPrinter = null;
+  public static PaperSource printerSource = null;
 
   public void addToQueue(String path)
   {
     printerQueue.add(path);
-  }
-
-  public void removeFromQueue()
-  {
-    printerQueue.remove();
   }
 
   public void startPrintQueue()
@@ -23,17 +33,36 @@ public class Printing
     while (printerQueue.size() != 0)
     {
       printSingle();
-      removeFromQueue();
     }
   }
 
   public static void printSingle()
   {
-    // making sure that this pushes
+    File file = new File(printerQueue.remove());
+    PDDocument document = Loader.loadPDF(file);
+    printJob.setPageable(new PDFPageable(document));
+    printJob.setPrintService(defaultPrinter);
+    printJob.print();
   }
 
-  public static void setPrinterProperties()
+
+  public static PrintService[] getPrintersAvailable()
   {
-    // TODO
+    return PrintServiceLookup.lookupPrintServices(null, null);  
+  }
+
+  public static void setPrinter(PrintService printer)
+  {
+    defaultPrinter = printer;
+  }
+
+  public static Set<PaperSource> getPrinterSources()
+  {
+    return PrinterAttributes.getSupportedPaperSources(); 
+  }
+
+  public static void setPrinterSource(PaperSource newSource)
+  {
+    printerSource = newSource;
   }
 }
