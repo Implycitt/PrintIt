b93eea1402ff8a9ce540501fb2b158d670003f0a
Implycitt <qgbordelon@gmail.com>
2025-12-03 01:40:21 -0600

    feat: label model, CRUD operations for database

--- src/main/java/com/implycitt/printit/Models/ItemLabel.java
+++ src/main/java/com/implycitt/printit/Models/ItemLabel.java
@@ -2,36 +2,33 @@ package com.implycitt.printit.Models;
 
 import java.util.Arrays;
 
-public class ItemLabel {
+public class ItemLabel
+{
   public Integer id;
-  public String[] names;
+  public String primaryName;
+  public String[] otherNames;
   public String url;
   public String[] subcategories;
   public String type;
 
-  public ItemLabel(Integer id, String[] names, String url, String[] subcategories, String type)
+  public ItemLabel(Integer id, String primaryName, String otherNames, String url, String subcategories, String type)
   {
     this.id = id;
-    this.names = names;
+    this.primaryName = primaryName;
+    this.otherNames = turnTextIntoArray(otherNames);
     this.url = url;
-    this.subcategories = subcategories;
+    this.subcategories = turnTextIntoArray(subcategories);
     this.type = type;
   }
 
-  public static String[] turnTextIntoArray(String text)
+  public String[] turnTextIntoArray(String text)
   {
     // arrays are not supported in sqlite so I will steal idea behind csv except with pipe
     return text.split("|");
   }
 
-  public static String turnArrayIntoText(String[] array)
+  public String turnArrayIntoText(String[] array)
   {
     return String.join("|", array);
-  }
-
-  public String toString()
-  {
-    return String.format("id: %s, name: %s, url: %s, subcategories: %s, type: %s",
-      this.id, this.names, this.url, Arrays.toString(this.subcategories), this.type);
   }
 }
